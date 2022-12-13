938b2233e0b1179dfaeb5e334cc06ad832ef9660
ppetar33 <ppetar33@gmail.com>
2022-12-13 20:26:43 +0100

    functionality added

--- src/app/@pages/at-a-glance/at-a-glance.component.ts
+++ src/app/@pages/at-a-glance/at-a-glance.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hoolio-glance',
@@ -12,6 +12,30 @@ export class AtAGlanceComponent implements OnInit {
 
   public ngOnInit(): void {
     this.fadeIn();
+    this.accordion();
+  }
+
+  public accordion(): void {
+    const openButton = document.querySelectorAll("[data-js='open']");
+    for (let i = 0; i < openButton.length; i++) {
+      openButton[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        openButton[i].classList.toggle("active");
+        var showTxt: any;
+        if(openButton[i].previousElementSibling?.className === "card__bottom-desc-wrapper"){
+          showTxt = openButton[i].previousElementSibling;
+          openButton[i].classList.toggle("close-glance");
+        }else{
+          showTxt = openButton[i].nextElementSibling;
+          openButton[i].classList.toggle("close");
+        }
+        if (showTxt?.style.maxHeight) {
+          showTxt.style.maxHeight = null;
+        } else {
+          showTxt.style.maxHeight = showTxt?.scrollHeight + "px";
+        }
+      });
+    }
   }
 
   public fadeIn(): void {
